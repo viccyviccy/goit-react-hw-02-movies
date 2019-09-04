54f0f3bce75c06163bde660035e098748c6dbe60
viccyviccy <iamviccyviccy@gmail.com>
2019-09-04 20:19:40 +0300

    completed movies HW

---
+++ build/precache-manifest.edb3ab697000051e17e5d7b9f4565a41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "050f283db18312453a0ec8bc68c7c360",
+    "url": "/goit-react-hw-02-movies/index.html"
+  },
+  {
+    "revision": "384edefb3e0745066a1b",
+    "url": "/goit-react-hw-02-movies/static/css/main.970d28c4.chunk.css"
+  },
+  {
+    "revision": "821f9e32393be5e29e3b",
+    "url": "/goit-react-hw-02-movies/static/js/2.49307853.chunk.js"
+  },
+  {
+    "revision": "384edefb3e0745066a1b",
+    "url": "/goit-react-hw-02-movies/static/js/main.86b12efa.chunk.js"
+  },
+  {
+    "revision": "0069d2c8e0e7d1c56d0e",
+    "url": "/goit-react-hw-02-movies/static/js/runtime~main.58f451ba.js"
+  }
+]);
